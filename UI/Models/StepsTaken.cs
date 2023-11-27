69aab15ac860110f994d0f6a9adbdff43bc127f1
Rob Vettor <robvet@microsoft.com>
2023-11-26 23:41:46 -0600

    first commit

---
+++ UI/Models/StepsTaken.cs
@@ -0,0 +1,12 @@
+﻿namespace AzureOpenAIChat.Models
+{
+    public class StepsTaken
+    {
+        public string thought { get; set; }
+        public string action { get; set; }
+        public Dictionary<string, string> action_variables { get; set; }
+        public string observation { get; set; }
+        public string final_answer { get; set; }
+        public string original_response { get; set; }
+    }
+}
---
+++ UI/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Markdig;
+
+namespace AzureOpenAIChat
+{
+    public static class StringExtensions
+    {
+        // Copyright (c) David Pine. All rights reserved.
+        private static readonly MarkdownPipeline s_pipeline = 
+            new MarkdownPipelineBuilder()
+            .ConfigureNewLine("\n")
+            .UseAdvancedExtensions()
+            .UseEmojiAndSmiley()
+            .UseSoftlineBreakAsHardlineBreak()
+            .Build();
+        public static string ToHtml(this string markdown) => 
+            string.IsNullOrWhiteSpace(markdown) is false
+            ? Markdown.ToHtml(markdown, s_pipeline)
+            : "";
+    }
+}
